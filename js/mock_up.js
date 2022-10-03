b39edbed6e6cc372d017eaa9f4be31236b2c5fe1
hogus24 <jangso4@naver.com>
2022-10-04 01:22:36 +0900

    프로필 사진 삽입

--- js/mock_up.js
+++ js/mock_up.js
@@ -1,33 +1,27 @@
 const hiddens = document.querySelectorAll(".hidden");
-// const thumbs = document.querySelectorAll(".hidden > img");
 hiddens.forEach(function (hidden) {
   hidden.addEventListener("mouseenter", function (e) {
     e.preventDefault();
     const tg = e.target;
     const tgH = tg.offsetHeight;
-    console.log(tgH); 
-    // a 높이 308
+    // offsetHeight -> .hidden 의 높이
     const tgThumb = tg.childNodes[1];
+    // childNodes[] = 자식요소 선택자, 자식의 인덱스 번호
     const tgThumbH = tgThumb.offsetHeight;
-    console.log(tgThumbH);
-    // img 높이 2006
+    // tgThumbH -> .hidden 의 자식요소, img 의 높이
     tgThumb.style.top = tgH - tgThumbH + "px";
-    console.log(tgThumb.style.top);
-    // 변화 img 높이 : a - img = 308 - 2006 = -1698
   });
   hidden.addEventListener("mouseout", function (e) {
     e.preventDefault();
     const tg = e.target;
-    console.log(tg);
     const tgH = tg.offsetHeight;
     console.log(tgH);
-    // img 높이 2006
+    // tgH -> .hidden 의 자식요소, img 의 높이
     const tgThumb = tg.parentNode;
-    console.log(tgThumb);
+    // parentNode = 부모요소 선택자
     const tgThumbH = tgThumb.offsetHeight;
     console.log(tgThumbH);
-    // a 높이 308
-    tgH.style.top = - tgH + tgThumbH + "px";
-    // 이미지 탑 : 
+    // tgThumbH -> .hidden 의 높이
+    tg.style.top = 0 + "px";
   });
 });
